c36506678dc50bb7408bb40ecc71237155e30407
jinx <550961784@qq.com>
2022-05-20 10:48:25 +0800

    feat: auth/login 数据校验 & 设置cookie

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express'
 import type { Request, Response } from 'express'
-import { validate, ValidationError } from 'class-validator'
+import { isEmpty, validate, ValidationError } from 'class-validator'
+import bcrypt from 'bcrypt'
+import cookie from 'cookie'
+import jwt from 'jsonwebtoken'
 import AppDataSource from '../data-source'
 import User from '../entity/User'
 
@@ -37,8 +40,58 @@ const register = async (req: Request, res: Response) => {
   }
 }
 
+const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body
+
+  const errors: { [key: string]: string } = {}
+
+  if (isEmpty(username)) {
+    errors.username = '用户名不能为空'
+  }
+  if (isEmpty(password)) {
+    errors.password = '密码不能为空'
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).json(errors)
+  }
+
+  try {
+    const user = await AppDataSource.manager.findOneBy(User, { username })
+    if (!user) {
+      return res.status(404).json({ error: '用户不存在' })
+    }
+
+    const passwordMatched = await bcrypt.compare(password, user.password)
+
+    if (!passwordMatched) {
+      return res.status(401).json({ error: '密码错误' })
+    }
+
+    const token = jwt.sign({ username }, process.env.JWT_SECRET)
+
+    res.set(
+      'Set-Cookie',
+      cookie.serialize('token', token, {
+        // https://www.npmjs.com/package/cookie#options
+        httpOnly: process.env.NODE_ENV === 'production', // 如果为 true, 浏览器无法使用 document.cookie 访问 cookie
+        secure: process.env.NODE_ENV === 'production', // 如果为true, 如果不是使用https连接，浏览器将不会发送cookie到服务器
+        sameSite: 'strict', // 只能本site能使用
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 过期时间
+        path: '/', // 表示所有路径都可以访问
+      })
+    )
+
+    return res.status(200).json(user)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json(error)
+  }
+}
+
 const router = Router()
 
 router.post('/register', register)
+router.post('/login', login)
 
 export default router
