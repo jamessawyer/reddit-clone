d4c6702b22b8681cd88579109599d9750b065669
jinx <550961784@qq.com>
2022-05-26 14:30:06 +0800

    utils(helpers): 添加makeId & slugify工具函数

---
+++ src/utlis/helpers.ts
@@ -0,0 +1,29 @@
+export function makeId(length: number): string {
+  let result = ''
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  const charactersLength = characters.length
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+  return result
+}
+
+// https://gist.github.com/codeguy/6684588#gistcomment-2759673
+export function slugify(str: string): string {
+  let newStr = str.replace(/^\s+|\s+$/g, '') // trim
+  newStr = newStr.toLowerCase()
+
+  // remove accents, swap ñ for n, etc
+  const from = 'àáäâèéëêìíïîòóöôùúüûñç·/_,:;'
+  const to = 'aaaaeeeeiiiioooouuuunc------'
+  for (let i = 0, l = from.length; i < l; i++) {
+    newStr = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i))
+  }
+
+  newStr = newStr
+    .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+    .replace(/-+/g, '-') // collapse dashes
+
+  return newStr
+}
